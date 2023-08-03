3c49a0cfb3eb77c0be8cfb7ed6bf3e6b96ee6f82
kirsten d <kirdanic@gmail.com>
2023-08-02 23:59:45 -0400

    Language

---
+++ src/main/java/com/melolingo/app/models/Language.java
@@ -0,0 +1,7 @@
+package com.melolingo.app.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
